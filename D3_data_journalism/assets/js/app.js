0862c94c595c67a04a5f7e4e66987d6c2a2af5dd
SSiva Raja <ishanku@gmail.com>
2020-04-07 19:44:04 -0400

    update app.js

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -23,6 +23,29 @@ var svgArea = d3.select("body").select("svg");
   var height = svgHeight - margin.top - margin.bottom;
   var width = svgWidth - margin.left - margin.right;
 
+  var currentX="poverty";
+  var currentY="healthcare";
+  var currentText = "abbr";
+
+
+// create scales
+function createScale(jData,axisName)
+{
+  var currentLinearScale = d3.scaleLinear()
+   .domain([d3.min(jData, d => d[axisName]*.5),d3.max(jData, d => d[axisName]*1.5)])
+   .range([0, width]);
+
+  return currentLinearScale;
+}
+
+// var xLinearScale = d3.scaleLinear()
+//    .domain([d3.min(jData, d => d[currentX]*.2),d3.max(jData, d => d[currentX]*1.2)])
+//    .range([0, width]);
+
+//  var yLinearScale = d3.scaleLinear()
+//    .domain([d3.min(jData, d => d[currentY]*.2), d3.max(jData, d => d[currentY]*1.2)])
+//    .range([height, 0]);
+
   // Append SVG element
   var svg = d3
     .select("#scatter")
@@ -30,65 +53,93 @@ var svgArea = d3.select("body").select("svg");
     .attr("height", svgHeight)
     .attr("width", svgWidth);
 
+
   // Append group element
   var chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
+function updateChart(jData,currentX,currentY,xLinearScale,yLinearScale){
+
+  var circlesGroup = chartGroup.selectAll("circle")
+  .data(jData)
+  .enter()
+  .append("circle")
+  .attr("cx", d => xLinearScale(d[currentX]))
+  .attr("cy", d => yLinearScale(d[currentY]))
+  .attr("r", "10")
+  .attr("fill", "gold")
+  .attr("stroke-width", "1")
+  .attr("stroke", "black")
+  .attr("opacity", ".9");
+
+  return circlesGroup
+}
+function updateText(jData,currentX,currentY,currentText,xLinearScale,yLinearScale){
+  var TextGroup = chartGroup.selectAll()
+  .data(jData)
+  .enter()
+  .append("text")
+  .text(d => (d[currentText]))
+  .attr("x", d => xLinearScale(d[currentX]))
+  .attr("y", d => yLinearScale(d[currentY]))
+  .style("font-size", "11px")
+  .style("text-anchor", "middle")
+  .style('fill', 'black')
+  .attr("opacity", ".5");
+
+  return TextGroup;
+}
+
+
 
 d3.csv("D3_data_journalism/assets/data/data.csv").then(function(jData) {
   
+  var xLinearScale = createScale(jData,currentX);
+  var yLinearScale = createScale(jData,currentY);
+
 
   jData.forEach(function(data) {
     data.poverty = +data.poverty;
-    data.healthcare= +data.healthcare;
+    data.healthcare = +data.healthcare;
+    data.age = +data.age;
+    data.income = +data.income;
+    data.smokes = +data.smokes;
+    data.obesity = +data.obesity;
   });
 
-  // create scales
-  var xLinearScale = d3.scaleLinear()
-    .domain(d3.extent(jData, d => d.poverty))
-    .range([0, width]);
-
-  var yLinearScale = d3.scaleLinear()
-    .domain([0, d3.max(jData, d => d.healthcare)])
-    .range([height, 0]);
+ 
 
   // create axes
   var xAxis = d3.axisBottom(xLinearScale).ticks(6);
   var yAxis = d3.axisLeft(yLinearScale).ticks(6);;
 
 
-  chartGroup.append("g")
+chartGroup.append("g")
   .attr("transform", `translate(0, ${height})`)
   .call(xAxis);
 
 chartGroup.append("g")
   .call(yAxis);
 
-  var circlesGroup = chartGroup.selectAll("circle")
-  .data(jData)
-  .enter()
-  .append("circle")
-  .attr("cx", d => xLinearScale(d.poverty))
-  .attr("cy", d => yLinearScale(d.healthcare))
-  .attr("r", "10")
-  .attr("fill", "gold")
-  .attr("stroke-width", "1")
-  .attr("stroke", "black");
 
-  var toolTip = d3.tip()
-        .attr("class", "tooltip")
-        .offset([80, -60])
-        .html(function(d) {
-          return (`<strong>${d.abbr}<strong><hr>${d.poverty}
-          medal(s) won`);
-        });
+updateChart(jData,currentX,currentY,xLinearScale,yLinearScale);
+updateText(jData,currentX,currentY,currentText,xLinearScale,yLinearScale);
 
-  circlesGroup.on("mouseover", function(d) {
-    toolTip.show(d, this);
-  })
-  // Step 4: Create "mouseout" event listener to hide tooltip
-    .on("mouseout", function(d) {
-      toolTip.hide(d);
-    });
+
+  // var toolTip = d3.tip()
+  //       .attr("class", "tooltip")
+  //       .offset([80, -60])
+  //       .html(function(d) {
+  //         return (`<strong>${d.abbr}<strong><hr>${d.poverty}
+  //         medal(s) won`);
+  //       });
+
+  // circlesGroup.on("mouseover", function(d) {
+  //   toolTip.show(d, this);
+  // })
+  // // Step 4: Create "mouseout" event listener to hide tooltip
+  //   .on("mouseout", function(d) {
+  //     toolTip.hide(d);
+  //   });
 
 });
